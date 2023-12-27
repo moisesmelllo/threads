aacb0ef23b929875611c13f8e4cce76cdcdc8937
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-12-26 22:56:19 -0300

    feat:  creating the layout

---
+++ src/components/shared/Topbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+function Topbar() {
+  return (
+    <nav className="topbar">
+      <Link href='/' className="flex items-center gap-4">
+        
+      </Link>
+    </nav>  
+  )
+}
+
+export default Topbar;
